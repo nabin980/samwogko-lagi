b667b39f0a0de639da461ab49238cec7375286d9
Shenrt <olinaveen98@gmail.com>
2024-02-22 14:55:51 +0545

    samyog

--- server/src/index.js
+++ server/src/index.js
@@ -1,71 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const http = require('http');
-const { Server } = require('socket.io');
-const Orders = require('./models/Orders');
-
-
+const express = require("express");
 const app = express();
+const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const Orders = require("./models/Orders");
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: "*",
   },
 });
+const cors = require("cors");
+const connect = require("./db/connect");
+const usersRouter = require("./routes/usersRouter");
+const loginRouter = require("./routes/loginRouter");
+const ordersRouter = require("./routes/ordersRouter");
+const itemsRouter = require("./routes/itemsRouter");
+const registerRouter = require("./routes/registerRouter");
+const sendMailRouter = require("./routes/sendMailRouter");
 
-const PORT = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
-
-mongoose.set('strictQuery', true);
-
-// Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/takeaway',{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// MongoDB models
-const Node = require('./models/Node');
-const Edge = require('./models/Edge');
-
-// Routes
-const nodesRoutes = require('./routes/nodesRoutes');
-const edgesRoutes = require('./routes/edgesRoutes');
-const shortestPathRoutes = require('./routes/shortestPathRoutes');
-const usersRouter = require('./routes/usersRouter');
-const loginRouter = require('./routes/loginRouter');
-const ordersRouter = require('./routes/ordersRouter');
-const itemsRouter = require('./routes/itemsRouter');
-const registerRouter = require('./routes/registerRouter');
-const sendMailRouter = require('./routes/sendMailRouter');
-
-// Socket.IO
-io.on('connection', (socket) => {
-  console.log('Socket is connected');
-  socket.on('orderRequest', async (orderRequest) => {
-    io.emit('orderRequest', orderRequest);
+require("dotenv").config();
+io.on("connection", (socket) => {
+  console.log("socket is connected");
+  socket.on("orderRequest", async (orderRequest) => {
+    io.emit("orderRequest", orderRequest);
     await Orders.findByIdAndUpdate(orderRequest.id, {
       orderStatus: orderRequest.status,
       orderStatusId: orderRequest.statusId,
     });
   });
 });
+app.use(express.json());
+app.use(cors());
+app.use(usersRouter);
+app.use(itemsRouter);
+app.use(ordersRouter);
+app.use(loginRouter);
+app.use(registerRouter);
+app.use(sendMailRouter);
 
-// REST API routes
-app.use('/api/nodes', nodesRoutes);
-app.use('/api/edges', edgesRoutes);
-app.use('/api/shortest-path', shortestPathRoutes);
-app.use('/api/users', usersRouter);
-app.use('/api/login', loginRouter);
-app.use('/api/orders', ordersRouter);
-app.use('/api/items', itemsRouter);
-app.use('/api/register', registerRouter);
-app.use('/api/send-mail', sendMailRouter);
+connect();
 
-// Start the server
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
--- server/src/routes/shortestPathRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/shortestPathRoutes.js
-const express = require('express');
-const router = express.Router();
-const shortestPathController = require('../controllers/shortestPathController');
-
-// Define the route for finding the shortest path
-router.get('/:start/:end', shortestPathController.findShortestPath);
-
-module.exports = router;
